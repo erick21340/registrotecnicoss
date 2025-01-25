c29a6defe87f0d05e7c68711fb0d0e35b3e369f1
erick21340 <Torot0086@gmail.com>
2025-01-25 13:11:35 -0900

    realizando el tecnicoviewmodel

--- app/src/main/java/com/example/registrotecnicos/presentation/screens/tecnico/TecnicoViewModel.kt
+++ app/src/main/java/com/example/registrotecnicos/presentation/screens/tecnico/TecnicoViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.registrotecnicos.presentation.screens.tecnico
+ package com.example.registrotecnicos.presentation.screens.tecnico
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
