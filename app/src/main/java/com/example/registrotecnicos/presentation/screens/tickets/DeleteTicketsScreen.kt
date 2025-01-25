d13e261967d11e976f13bcc5b44e41ef818ec884
erick21340 <Torot0086@gmail.com>
2025-01-25 13:14:57 -0900

    realizando el DeleteTicketScreen

--- app/src/main/java/com/example/registrotecnicos/presentation/screens/tickets/DeleteTicketsScreen.kt
+++ app/src/main/java/com/example/registrotecnicos/presentation/screens/tickets/DeleteTicketsScreen.kt
@@ -1,4 +1,4 @@
-package com.example.registrotecnicos.presentation.screens.tickets
+ package com.example.registrotecnicos.presentation.screens.tickets
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
