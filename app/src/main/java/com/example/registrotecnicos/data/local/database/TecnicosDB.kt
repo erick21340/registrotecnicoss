7e05382db89b1b41a51287566f8fb1a169cc3297
erick21340 <Torot0086@gmail.com>
2025-01-25 21:32:18 -0900

    actualizado el tecnicoDb

--- app/src/main/java/com/example/registrotecnicos/data/local/database/TecnicosDB.kt
+++ app/src/main/java/com/example/registrotecnicos/data/local/database/TecnicosDB.kt
@@ -2,8 +2,10 @@ package com.example.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.registrotecnicos.data.local.dao.MensajeDao
 import com.example.registrotecnicos.data.local.dao.TecnicoDao
 import com.example.registrotecnicos.data.local.dao.TicketDao
+import com.example.registrotecnicos.data.local.entities.MensajeEntity
 import com.example.registrotecnicos.data.local.entities.TecnicoEntity
 import com.example.registrotecnicos.data.local.entities.TicketEntity
 
@@ -11,11 +13,13 @@ import com.example.registrotecnicos.data.local.entities.TicketEntity
     entities = [
         TecnicoEntity::class,
         TicketEntity::class,
+        MensajeEntity::class,
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class  TecnicosDB : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
+    abstract fun mensajeDao(): MensajeDao
 }
