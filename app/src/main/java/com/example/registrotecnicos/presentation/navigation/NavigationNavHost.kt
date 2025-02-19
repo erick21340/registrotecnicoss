e77dc1eb7a30907d9d91a803cd64b79e05a22050
erick21340 <Torot0086@gmail.com>
2025-02-19 01:04:15 -0900

    agreguee  la pantalla de articulos

--- app/src/main/java/com/example/registrotecnicos/presentation/navigation/NavigationNavHost.kt
+++ app/src/main/java/com/example/registrotecnicos/presentation/navigation/NavigationNavHost.kt
@@ -10,6 +10,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.registrotecnicos.presentation.component.NavigationDrawer
+import com.example.registrotecnicos.presentation.screens.articulo.CreateArticulosScreen
+import com.example.registrotecnicos.presentation.screens.articulo.DeleteArticulosScreen
+import com.example.registrotecnicos.presentation.screens.articulo.EditArticulosScreen
+import com.example.registrotecnicos.presentation.screens.articulo.IndexArticulosScreen
 import com.example.registrotecnicos.presentation.screens.home.HomeScreen
 import com.example.registrotecnicos.presentation.screens.responde.MensajeScreen
 import com.example.registrotecnicos.presentation.screens.tecnico.IndexTecnicoScreen
@@ -17,6 +21,8 @@ import com.example.registrotecnicos.presentation.screens.tickets.CreateTicketScr
 import com.example.registrotecnicos.presentation.screens.tickets.DeleteTicketsScreen
 import com.example.registrotecnicos.presentation.screens.tickets.EditTicketScreen
 import com.example.registrotecnicos.presentation.screens.tickets.IndexTicketScreen
+
+
 
 
 @Composable
@@ -44,7 +50,6 @@ fun NavigationNavHost(
                     onDrawerToggle = { isDrawerVisible.value = !isDrawerVisible.value },
                 )
             }
-
 
             // Tickets
             composable<Screen.IndexTicketScreen> {
@@ -105,6 +110,68 @@ fun NavigationNavHost(
                 }
             }
 
+
+            // Articulos
+
+            composable<Screen.IndexArticulosScreen> {
+                IndexArticulosScreen(
+                    onDrawerToggle = { isDrawerVisible.value = !isDrawerVisible.value },
+                    onCreateArticulo = {
+                        navHostController.navigate(Screen.CreateArticulosScreen)
+                    },
+                    onEditArticulo = {
+                        navHostController.navigate(Screen.EditArticulosScreen(it))
+                    },
+                    onDeleteArticulo = {
+                        navHostController.navigate(Screen.DeleteArticulosScreen(it))
+                    }
+                )
+            }
+
+            composable<Screen.CreateArticulosScreen> {
+                CreateArticulosScreen(
+                    onDrawerToggle = {
+                        isDrawerVisible.value = !isDrawerVisible.value
+                    },
+                    goToArticulo = {
+                        navHostController.navigate(Screen.IndexArticulosScreen)
+                    }
+                )
+            }
+
+            composable<Screen.EditArticulosScreen> { backStackEntry ->
+                val articuloId = backStackEntry.arguments?.getInt("articuloId")
+                if (articuloId != null) {
+
+                    EditArticulosScreen(
+                        articuloId = articuloId,
+                        onDrawerToggle = {
+                            isDrawerVisible.value = !isDrawerVisible.value
+                        },
+                        goToArticulo = {
+                            navHostController.navigate(Screen.IndexArticulosScreen)
+                        }
+                    )
+                }
+            }
+
+            composable<Screen.DeleteArticulosScreen> { backStackEntry ->
+                val articuloId = backStackEntry.arguments?.getInt("articuloId")
+                if (articuloId != null) {
+
+                    DeleteArticulosScreen(
+                        articuloId = articuloId,
+                        onDrawerToggle = {
+                            isDrawerVisible.value = !isDrawerVisible.value
+                        },
+                        goToArticulo = {
+                            navHostController.navigate(Screen.IndexArticulosScreen)
+                        }
+                    )
+                }
+            }
+
+
             composable<Screen.IndexmensajeCreen> {
                 MensajeScreen(
 
@@ -114,6 +181,7 @@ fun NavigationNavHost(
                     }
                 )
             }
+
 
         }
     }
@@ -130,6 +198,7 @@ fun NavigationNavHost(
                 "Tecnicos" -> navHostController.navigate(Screen.IndexTecnicoScreen)
                 "Tickets" -> navHostController.navigate(Screen.IndexTicketScreen)
                 "Mensaje" -> navHostController.navigate(Screen.IndexmensajeCreen)
+                "Articulos" -> navHostController.navigate(Screen.IndexArticulosScreen)
             }
             isDrawerVisible.value = false
         },
