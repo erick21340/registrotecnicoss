a806558104ff1f98058d8b61f72c3da1111ff5c6
erick21340 <Torot0086@gmail.com>
2025-02-19 01:01:30 -0900

    agreguee ArticuloDto

---
+++ app/src/main/java/com/example/registrotecnicos/data/remote/dto/ArticuloDto.kt
@@ -0,0 +1,9 @@
+package com.example.registrotecnicos.remote.dto
+
+class ArticuloDto(
+    val articuloId: Int,
+    val descripcion: String,
+    val costo: Double,
+    val ganancia: Double,
+    val precio: Double
+)
