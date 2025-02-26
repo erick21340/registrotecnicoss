be82aafb848f3a4a11fb569cd052b74a8f82fc8a
erick21340 <Torot0086@gmail.com>
2025-02-25 21:26:41 -0900

    agreguee  articuloDb

--- app/src/main/java/com/example/registrotecnicos/data/local/database/TecnicosDB.kt
+++ app/src/main/java/com/example/registrotecnicos/data/local/database/TecnicosDB.kt
@@ -2,24 +2,32 @@ package com.example.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.registrotecnicos.data.local.dao.ArticuloDao
 import com.example.registrotecnicos.data.local.dao.MensajeDao
 import com.example.registrotecnicos.data.local.dao.TecnicoDao
 import com.example.registrotecnicos.data.local.dao.TicketDao
+
+import com.example.registrotecnicos.data.local.entities.ArticuloEntity
 import com.example.registrotecnicos.data.local.entities.MensajeEntity
 import com.example.registrotecnicos.data.local.entities.TecnicoEntity
 import com.example.registrotecnicos.data.local.entities.TicketEntity
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
 
 @Database(
     entities = [
         TecnicoEntity::class,
         TicketEntity::class,
         MensajeEntity::class,
+        ArticuloEntity:: class,
     ],
-    version = 3,
+    version = 5,
     exportSchema = false
 )
 abstract class  TecnicosDB : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
     abstract fun mensajeDao(): MensajeDao
+    abstract fun ArticuloDao(): ArticuloDao
 }
